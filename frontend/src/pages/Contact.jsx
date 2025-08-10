c8683ec2fa7d229441d2f353545e6bca26265fd2
HARIHARAN R <144225049+harichopper@users.noreply.github.com>
2025-08-10 12:41:01 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact','https://arta-frontend-65ui.vercel.app', {
+      const response = await fetch( 'https://arta-frontend-65ui.vercel.app', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
